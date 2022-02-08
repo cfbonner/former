a2cbe5ce05a0dbc687393c26e3fcb9dfd31214df
cfbonner <cfbonner@gmail.com>
2022-02-08 21:47:17 +0000

    Add slugs to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,12 @@
 class Page < ApplicationRecord
+  validates :title, :slug, presence: true
+  before_save :parameterize_slug
+
+  def parameterize_slug
+    self.slug = self.slug.parameterize unless self.slug.nil?
+  end
+
+  def to_param
+    slug
+  end
 end
